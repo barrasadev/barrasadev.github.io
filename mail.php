e1a8102b64b59d5797716d736f57cfdfdb4a699e
barrasa <barrasadev@gmail.com>
2021-07-28 00:30:24 +0200

    subiendo barrasa web

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "contact@barrasa.dev";
+$sitename = "Barrasa Website";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
